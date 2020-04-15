16ee301596868094665a583ef372f8503e0e0a3d
dkashikar <dinesh@kashikar.net>
2020-04-15 23:25:09 +0530

    Init

---
+++ docs/precache-manifest.29fb3750b26d3b197da30bb281904733.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b07b1c050c88e885b7a34a6b5be430f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d434ebf9fd7727a26085",
+    "url": "/static/css/2.5f01b180.chunk.css"
+  },
+  {
+    "revision": "c9887935d25f6cd0d5d3",
+    "url": "/static/css/main.e6b63007.chunk.css"
+  },
+  {
+    "revision": "d434ebf9fd7727a26085",
+    "url": "/static/js/2.d434ebf9.chunk.js"
+  },
+  {
+    "revision": "45deb4f1dbeaf409e2523d15502323f0",
+    "url": "/static/js/2.d434ebf9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c9887935d25f6cd0d5d3",
+    "url": "/static/js/main.c9887935.chunk.js"
+  },
+  {
+    "revision": "74f5c71c2095b5ac8acb",
+    "url": "/static/js/runtime~main.74f5c71c.js"
+  }
+]);
